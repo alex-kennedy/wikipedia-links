8f21eb92127ac73d460634901fef29c2bf30a157
alex-kennedy <apkennedy@me.com>
2019-03-29 17:21:59 +1300

    Binary search for a fixed width file update

--- src/processing/bsearch.py
+++ src/processing/bsearch.py
@@ -2,34 +2,55 @@ import os
 import time
 
 class BinarySearchFile:
-    def __init__(self, file_name, num_lines, line_len):
+    def __init__(self, file_name, n_lines, line_len, key_len):
         self.file_name = file_name
-        self.num_lines = num_lines
+        self.n_lines = n_lines
         self.line_len = line_len
+        self.key_len = key_len
 
         self.f = open(file_name, 'rb')
         self.size = os.stat(file_name).st_size
 
     def __getitem__(self, line_num):
         self.f.seek(self.line_len * line_num)
-        return self.f.readline()
+        line = self.f.readline()
+        print(line[self.key_len:].strip())
+        return line[:self.key_len], line[self.key_len:]
 
-    def search(self, key):
-        return self.binary_search(key, 0, self.num_lines - 1)
+    def __enter__(self):
+        return self
 
-    def binary_search(self, key, low, high):
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.f.close()
+
+    def _binary_search(self, key, low, high):
         if low > high:
             return -1
 
         middle = (low + high) // 2
+        result, value = self[middle]
 
-        if self[middle] == key:
-            return middle
+        if result == key:
+            return value
 
-        if self[middle] > key:
-            return self.binary_search(key, low, middle - 1)
+        if result > key:
+            return self._binary_search(key, low, middle - 1)
         else:
-            return self.binary_search(key, middle + 1, high)
+            return self._binary_search(key, middle + 1, high)
+
+    def search(self, key):
+        return self._binary_search(key, 0, self.n_lines - 1)
+
+    def search_many(self, keys):
+        keys.sort()
+        results = []
+
+        low = 0
+        for key in keys:
+            i = self._binary_search(key, low, self.n_lines - 1)
+            if i != -1:
+                low = i
+            results.append(i)
 
 
 def temp():
