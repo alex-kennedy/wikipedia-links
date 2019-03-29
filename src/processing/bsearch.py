293f404b2dda6100d428e637e68d0be4d287a80f
alex-kennedy <apkennedy@me.com>
2019-03-29 18:39:02 +1300

    Improved binary search to variable line length files

--- src/processing/bsearch.py
+++ src/processing/bsearch.py
@@ -1,21 +1,29 @@
 import os
 import time
 
+
 class BinarySearchFile:
-    def __init__(self, file_name, n_lines, line_len, key_len):
+    def __init__(self,
+                 file_name,
+                 create_index=True,
+                 n_lines=None,
+                 line_len=None,
+                 key_len=None):
         self.file_name = file_name
-        self.n_lines = n_lines
-        self.line_len = line_len
-        self.key_len = key_len
+        self.f = open(file_name, encoding='utf-8')
 
-        self.f = open(file_name, 'rb')
-        self.size = os.stat(file_name).st_size
-
-    def __getitem__(self, line_num):
-        self.f.seek(self.line_len * line_num)
-        line = self.f.readline()
-        print(line[self.key_len:].strip())
-        return line[:self.key_len], line[self.key_len:]
+        if create_index:
+            self.index = self._create_byte_index()
+            self.n_lines = len(self.index)
+            self.get_line = self._index_get
+        else:
+            if not (n_lines and line_len and key_len):
+                raise ValueError('If create_index is not True, n_lines, '
+                                 'line_len, and key_len must be provided.')
+            self.n_lines = n_lines
+            self.line_len = line_len
+            self.key_len = key_len
+            self.get_line = self._fixed_get
 
     def __enter__(self):
         return self
@@ -23,12 +31,33 @@ class BinarySearchFile:
     def __exit__(self, exception_type, exception_value, traceback):
         self.f.close()
 
+    def __getitem__(self, line_num):
+        return self.get_line(line_num)
+
+    def _create_byte_index(self):
+        locs = [0]
+        with open(self.file_name, 'rb') as f:
+            for _ in f:
+                locs.append(f.tell())
+        return locs[:-1]
+
+    def _index_get(self, line_num):
+        self.f.seek(self.index[line_num])
+        line = self.f.readline()
+        i = line.rfind(',')
+        return line[:i], line[i + 1:]
+
+    def _fixed_get(self, line_num):
+        self.f.seek(self.line_len * line_num)
+        line = self.f.readline()
+        return line[:self.key_len], line[self.key_len:]
+
     def _binary_search(self, key, low, high):
         if low > high:
             return -1
 
         middle = (low + high) // 2
-        result, value = self[middle]
+        result, value = self.get_line(middle)
 
         if result == key:
             return value
@@ -64,16 +93,3 @@ def temp():
 
         for string in sorted(strings):
             out.write(string + '\n')
-
-if __name__ == '__main__':
-    # temp()
-    # with open('src/processing/sorted_file.txt') as f:
-    #     f.seek(58)
-    #     print(f.read())
-
-    bsearch = BinarySearchFile('src/processing/sorted_file.txt', 1000000, 58)
-    
-    start = time.time()
-    for i in range(100000):
-        bsearch.search(b'ptgikejzsanybrpjoqeuqzknjedcqyaqfzoddcdmdytnlpsrgppwgtklp\n')
-    print(time.time() - start)
